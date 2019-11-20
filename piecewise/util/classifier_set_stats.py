e19d651928d01fe705a05e20384200a5b0b3e3ee
Jordan Bishop <jordanbishop26@gmail.com>
2019-11-20 16:22:03 +1000

    Add more min/max calcs to summarisation of population

--- piecewise/util/classifier_set_stats.py
+++ piecewise/util/classifier_set_stats.py
@@ -56,23 +56,51 @@ def summarise_population(population, rule_repr, time_step):
     summary = {}
     summary["num_micros"] = population.num_micros()
     summary["num_macros"] = population.num_macros()
+    summary["min_prediction"] = calc_summary_stat(population, "min",
+                                                  "prediction")
     summary["mean_prediction"] = calc_summary_stat(population, "mean",
                                                    "prediction")
+    summary["max_prediction"] = calc_summary_stat(population, "max",
+                                                  "prediction")
+    summary["min_error"] = calc_summary_stat(population, "min", "error")
     summary["mean_error"] = calc_summary_stat(population, "mean", "error")
+    summary["max_error"] = calc_summary_stat(population, "max", "error")
+    summary["min_fitness"] = calc_summary_stat(population, "min", "fitness")
     summary["mean_fitness"] = calc_summary_stat(population, "mean", "fitness")
+    summary["max_fitness"] = calc_summary_stat(population, "max", "fitness")
+    summary["min_time_stamp"] = calc_summary_stat(population, "min",
+                                                  "time_stamp")
     summary["mean_time_stamp"] = calc_summary_stat(population, "mean",
                                                    "time_stamp")
+    summary["max_time_stamp"] = calc_summary_stat(population, "max",
+                                                  "time_stamp")
+    summary["min_experience"] = calc_summary_stat(population, "min",
+                                                  "experience")
     summary["mean_experience"] = calc_summary_stat(population, "mean",
                                                    "experience")
+    summary["max_experience"] = calc_summary_stat(population, "max",
+                                                  "experience")
+    summary["min_action_set_size"] = \
+        calc_summary_stat(population, "min", "action_set_size")
     summary["mean_action_set_size"] = \
         calc_summary_stat(population, "mean", "action_set_size")
+    summary["max_action_set_size"] = \
+        calc_summary_stat(population, "max", "action_set_size")
     summary["min_numerosity"] = \
         calc_summary_stat(population, "min", "numerosity")
     summary["max_numerosity"] = \
         calc_summary_stat(population, "max", "numerosity")
+    summary["min_generality"] = min([
+        classifier.generality_as_percentage(rule_repr)
+        for classifier in population
+    ])
     summary["mean_generality"] = sum([
         classifier.generality_as_percentage(rule_repr)
         for classifier in population
     ]) / population.num_micros()
+    summary["max_generality"] = max([
+        classifier.generality_as_percentage(rule_repr)
+        for classifier in population
+    ])
     summary["time_step"] = time_step
     return summary
