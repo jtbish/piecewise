56e33ede91bbbf371676ac1fdb459d322cca12f1
Jordan Bishop <jordanbishop26@gmail.com>
2019-11-22 11:46:26 +1000

    population: rethink wiring of public and atomic methods

--- piecewise/dtype/classifier_set/population.py
+++ piecewise/dtype/classifier_set/population.py
@@ -6,13 +6,13 @@ from .abstract_classifier_set import AbstractClassifierSet, verify_membership
 from .population_state import PopulationState
 
 
-def delete_after(method):
-    """Decorator to perform deletions after duplications or insertions into the
+def delete_after(public_method):
+    """Decorator to perform deletions after performing an operation on the
     population, in order to keep the size of the population at or below
     its upper bound."""
-    @functools.wraps(method)
+    @functools.wraps(public_method)
     def _delete_after(self, *args, **kwargs):
-        result = method(self, *args, **kwargs)
+        result = public_method(self, *args, **kwargs)
         self._perform_necessary_deletions()
         assert self._MIN_MICROS <= self.num_micros() <= self._max_micros
         return result
@@ -25,12 +25,12 @@ def track_state_change(atomic_method):
     microclassifiers) when performing atomic operations on the population.
     """
     @functools.wraps(atomic_method)
-    def _track_state_change(self, classifier, track_label):
+    def _track_state_change(self, *args, track_label=None):
         micros_before = self.num_micros()
-        result = atomic_method(self, classifier, track_label)
+        result = atomic_method(self, *args, track_label)
         micros_after = self.num_micros()
-        micros_diff = micros_after - micros_before
-        self._state.update(track_label, micros_diff)
+        micros_delta = micros_after - micros_before
+        self._state.update(track_label, micros_delta)
         return result
 
     return _track_state_change
@@ -56,7 +56,7 @@ class Population(AbstractClassifierSet):
         XCS' (Butz and Wilson, 2002)."""
         was_absorbed = self._try_to_absorb(classifier)
         if not was_absorbed:
-            self._atomic_insert_new(classifier, track_label)
+            self._atomic_insert_new(classifier, track_label=track_label)
 
     def _try_to_absorb(self, classifier):
         for member in self._members:
@@ -66,21 +66,25 @@ class Population(AbstractClassifierSet):
         return False
 
     def _absorb(self, absorbee, absorber):
-        for _ in range(absorbee.numerosity):
-            self._atomic_copy_existing(absorber, "absorption")
-
-    @verify_membership
-    def replace(self, replacee, replacer, track_label=None):
-        self._members.remove(replacee)
-        self.duplicate(replacer,
-                       num_copies=replacee.numerosity,
-                       track_label=track_label)
+        num_copies = absorbee.numerosity
+        self._atomic_copy_existing(absorber,
+                                   num_copies,
+                                   track_label="absorption")
 
     @delete_after
     @verify_membership
     def duplicate(self, classifier, num_copies=1, track_label=None):
-        for _ in range(num_copies):
-            self._atomic_copy_existing(classifier, track_label)
+        self._atomic_copy_existing(classifier,
+                                   num_copies,
+                                   track_label=track_label)
+
+    @verify_membership
+    def replace(self, replacee, replacer, track_label=None):
+        self._atomic_remove_whole(replacee)
+        num_copies = replacee.numerosity
+        self._atomic_copy_existing(replacer,
+                                   num_copies,
+                                   track_label=track_label)
 
     def _perform_necessary_deletions(self):
         """DELETE FROM POPULATION function from 'An Algorithmic Description of
@@ -96,7 +100,7 @@ class Population(AbstractClassifierSet):
             for _ in range(num_deletions):
                 classifier_to_delete = self._deletion_strat(self)
                 self._atomic_remove_single_copy(classifier_to_delete,
-                                                "deletion")
+                                                track_label="deletion")
 
     # Atomic operations
     @track_state_change
@@ -104,8 +108,15 @@ class Population(AbstractClassifierSet):
         self._members.append(new_classifier)
 
     @track_state_change
-    def _atomic_copy_existing(self, existing_classifier, track_label):
-        existing_classifier.numerosity += 1
+    def _atomic_copy_existing(self, existing_classifier, num_copies,
+                              track_label):
+        assert num_copies >= 1
+        for _ in range(num_copies):
+            existing_classifier.numerosity += 1
+
+    @track_state_change
+    def _atomic_remove_whole(self, classifier, track_label):
+        self._members.remove(classifier)
 
     @track_state_change
     def _atomic_remove_single_copy(self, existing_classifier, track_label):
