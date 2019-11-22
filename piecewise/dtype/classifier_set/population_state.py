1e4f209537cdd95fac79c6e8146b643d72e30038
Jordan Bishop <jordanbishop26@gmail.com>
2019-11-22 14:16:56 +1000

    pop state: replace concept of change with micros delta

--- piecewise/dtype/classifier_set/population_state.py
+++ piecewise/dtype/classifier_set/population_state.py
@@ -5,15 +5,13 @@ class PopulationState:
     """Keeps track of changes made to the state of a Population instance.
 
     Modifications are measured in units of *number of microclassifiers*.
-    E.g. if the state has recorded that five deletions have been performed,
-    then five microclassifiers have been deleted from the population since its
-    creation."""
+    """
     def __init__(self):
         self._state_changes = defaultdict(lambda: 0)
 
     def query(self):
         return self._state_changes
 
-    def update(self, track_label, change):
+    def update(self, track_label, micros_delta):
         if track_label is not None:
-            self._state_changes[track_label] += change
+            self._state_changes[track_label] += micros_delta
