28cc989b5cf29e5f545944d110d02410a6ae58ee
Jordan Bishop <jordanbishop26@gmail.com>
2019-11-27 16:56:32 +1000

    Changed sub monitor internals to lists

--- piecewise/monitor/sub_monitor.py
+++ piecewise/monitor/sub_monitor.py
@@ -1,9 +1,9 @@
 import abc
-from collections import OrderedDict, namedtuple
-
-import matplotlib.pyplot as plt
+from collections import namedtuple
 
 from piecewise.util.classifier_set_stats import calc_summary_stat
+
+EpochData = namedtuple("EpochData", ["epoch_num", "recorded_data"])
 
 
 class AbstractSubMonitor(metaclass=abc.ABCMeta):
@@ -15,67 +15,35 @@ class AbstractSubMonitor(metaclass=abc.ABCMeta):
     def query(self):
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def report(self):
-        raise NotImplementedError
-
 
 class TrainingPerformanceSubMonitor(AbstractSubMonitor):
     def __init__(self):
-        self._training_performance_history = OrderedDict()
+        self._training_performance_history = []
 
     def update(self, lcs, epoch_num):
         training_performance = lcs.calc_training_performance()
-        self._training_performance_history[epoch_num] = training_performance
+        self._training_performance_history.append(
+            EpochData(epoch_num, training_performance))
 
     def query(self):
         return self._training_performance_history
 
-    def report(self):
-        plt.figure()
-        epoch_nums = list(self._training_performance_history.keys())
-        training_performance_results = \
-            list(self._training_performance_history.values())
-        plt.plot(epoch_nums, training_performance_results)
-        plt.xlabel("Epoch number")
-        plt.ylabel("Training set performance")
-        plt.title("Training set performance vs. time")
-        plt.savefig("training_performance.png")
-
 
 class PopulationSizeSubMonitor(AbstractSubMonitor):
     def __init__(self):
-        self._population_size_history = OrderedDict()
+        self._population_size_history = []
 
     def update(self, lcs, epoch_num):
         num_micros = lcs.population.num_micros()
         num_macros = lcs.population.num_macros()
-        self._population_size_history[epoch_num] = {
-            "num micros": num_micros,
-            "num macros": num_macros
-        }
+        self._population_size_history.append(
+            EpochData(epoch_num, {
+                "num_micros": num_micros,
+                "num_macros": num_macros
+            }))
 
     def query(self):
         return self._population_size_history
-
-    def report(self):
-        plt.figure()
-        epoch_nums = list(self._population_size_history.keys())
-        micros_values = [
-            history["num micros"]
-            for history in self._population_size_history.values()
-        ]
-        macros_values = [
-            history["num macros"]
-            for history in self._population_size_history.values()
-        ]
-        plt.plot(epoch_nums, micros_values, label="num micros")
-        plt.plot(epoch_nums, macros_values, label="num macros")
-        plt.xlabel("Epoch number")
-        plt.ylabel("Population size")
-        plt.title("Population size vs. time")
-        plt.legend()
-        plt.savefig("pop_size.png")
 
 
 ClassifierSetStat = namedtuple("ClassifierSetStat",
@@ -85,11 +53,11 @@ ClassifierSetStat = namedtuple("ClassifierSetStat",
 class PopulationStatisticsSubMonitor(AbstractSubMonitor):
     def __init__(self, stats_to_monitor):
         self._stats_to_monitor = stats_to_monitor
-        self._stat_summary_history = OrderedDict()
+        self._stat_summary_history = []
 
     def update(self, lcs, epoch_num):
         summary = self._gen_summary(lcs.population)
-        self._stat_summary_history[epoch_num] = summary
+        self._stat_summary_history.append(EpochData(epoch_num, summary))
 
     def _gen_summary(self, population):
         summary = {}
@@ -109,21 +77,16 @@ class PopulationStatisticsSubMonitor(AbstractSubMonitor):
     def query(self):
         return self._stat_summary_history
 
-    def report(self):
-        pass
-
 
 class PopulationOperationsSubMonitor(AbstractSubMonitor):
     def __init__(self):
-        self._population_operations_history = OrderedDict()
+        self._population_operations_history = []
 
     def update(self, lcs, epoch_num):
         population = lcs.population
-        operations_state = population.get_operations_state()
-        self._population_operations_history[epoch_num] = operations_state
+        pop_ops = population.operations()
+        self._population_operations_history.append(
+            EpochData(epoch_num, pop_ops))
 
     def query(self):
         return self._population_operations_history
-
-    def report(self):
-        pass
